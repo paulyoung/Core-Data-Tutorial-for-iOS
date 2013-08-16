0f629eff90ff1bca8ef132ddf5804ffa2d115d44
Paul Young <paulyoungonline@gmail.com>
2013-08-15 20:56:41 -0400

    Added table view controller subclass.

---
+++ Locations/Locations/LOCTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  LOCTableViewController.h
+//  Locations
+//
+//  Created by Paul Young on 15/08/2013.
+//  Copyright (c) 2013 Paul Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface LOCTableViewController : UITableViewController
+
+@end
